47667eb3e53c45987929839a08b1c036af453f70
tohirjonovhadyatilo <mobiletillopubg@gmail.com>
2025-04-20 17:41:35 +0500

    feat: add Home page and update navigation from Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
+        
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<h1>Welcome Home!</h1>} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       alert("Login successful!");
-      navigate("/");
+      navigate("/home");
     } catch (error) {
       alert(error.message);
     }
@@ -24,7 +24,7 @@ const Login = () => {
     try {
       await signInWithPopup(auth, provider);
       alert("Google login successful!");
-      navigate("/");
+      navigate("/home");
     } catch (error) {
       alert(error.message);
     }
